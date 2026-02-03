15c9d8f30396a8a6c77f1d7561b55385826e80d5
Gabriel-Gerhardt <gabrielgerhardt27@gmail.com>
2026-02-03 15:02:25 -0300

    done

--- AI/impl/ingestion/csvFormatter.py
+++ AI/impl/ingestion/csvFormatter.py
@@ -1,13 +1,32 @@
+import os
+
 import pandas as pd
+from github import Github, Auth
+from dotenv import load_dotenv
 
-
+load_dotenv()
 def format_csv(name):
+    connectGithub()
     df = pd.read_csv(
         '../../../dataset/raw/'+name,
         usecols=["URL","Identifier","FileInfo",
                 "n_microservices","Application Type",
                 "Application Purpose","Developed by"])
+    df = df[df['Developed by'].str.contains("Industry", na=False)]
+    repoLinksDf= df
+
+    repo
 
     df.to_csv('../../../dataset/formatted/'+name)
+
+def connectGithub():
+    token = os.getenv("GITHUB_TOKEN")
+
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+    g.get_user().login
+
+
+
 
 format_csv("MicroservicesDataset.csv")
--- AI/impl/main.py
+++ AI/impl/main.py
@@ -1,19 +1,10 @@
-import os
-
-from dotenv import load_dotenv
 from langchain_community.document_loaders.csv_loader import CSVLoader
-
-
-#from AI.impl.agents.agent import Agent
 
 
 def main():
     loader = CSVLoader(
         file_path="../../dataset/formatted/MicroservicesDataset.csv",
     )
-    load_dotenv()
-    key = os.getenv("API_KEY")
     for document in loader.lazy_load():
         print(document)
-    print(key)
 main()

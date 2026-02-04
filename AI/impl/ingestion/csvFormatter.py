32760f5dceddf40dbe09509bbe1f20cd4488e376
Gabriel-Gerhardt <gabrielgerhardt27@gmail.com>
2026-02-04 10:26:34 -0300

    feat: git crawler to clone all repos

--- AI/impl/ingestion/csvFormatter.py
+++ AI/impl/ingestion/csvFormatter.py
@@ -1,32 +1,53 @@
 import os
 
+import github
 import pandas as pd
 from github import Github, Auth
 from dotenv import load_dotenv
 
+from AI.impl.integration.github.gitCrawler import GitCrawler
+
 load_dotenv()
 def format_csv(name):
-    connectGithub()
+    all_code = []
+    crawler = GitCrawler()
     df = pd.read_csv(
         '../../../dataset/raw/'+name,
         usecols=["URL","Identifier","FileInfo",
                 "n_microservices","Application Type",
                 "Application Purpose","Developed by"])
     df = df[df['Developed by'].str.contains("Industry", na=False)]
-    repoLinksDf= df
 
-    repo
+    for _, row in df.iterrows():
+        crawler.repo_url = row["URL"]
+        repo_df = reconstruct_df(crawler, row)
+        if not repo_df.empty:
+            all_code.append(repo_df)
+    final_df = pd.concat(all_code, ignore_index=True)
+    print(final_df)
 
-    df.to_csv('../../../dataset/formatted/'+name)
-
-def connectGithub():
-    token = os.getenv("GITHUB_TOKEN")
-
-    auth = Auth.Token(token)
-    g = Github(auth=auth)
-    g.get_user().login
+    final_df.to_csv('../../../dataset/formatted/'+name)
 
 
 
-
+def reconstruct_df(crawler:GitCrawler, meta_row):
+    repo_files = crawler.crawl_repo_code()
+    rows = []
+    for p in repo_files:
+        try:
+            content = p.read_text(errors="ignore")
+        except Exception:
+            continue
+        rows.append({
+            "URL": meta_row["URL"],
+            "Identifier": meta_row["Identifier"],
+            "FileInfo": meta_row["FileInfo"],
+            "n_microservices": meta_row["n_microservices"],
+            "Application Type": meta_row["Application Type"],
+            "Application Purpose": meta_row["Application Purpose"],
+            "Developed by": meta_row["Developed by"],
+            "path": str(p),
+            "code": content
+        })
+    return pd.DataFrame(rows)
 format_csv("MicroservicesDataset.csv")
---
+++ AI/impl/integration/github/gitCrawler.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+from pathlib import Path
+
+from github import Github
+from pydantic import BaseModel
+from typing import Any, Optional, List
+
+TARGET_DIRS = {"src", "app", "apps", "service", "services", "backend", "api", "server"}
+FILES = {"dockerfile"}
+IGNORE_DIRS = {".git", "target", "build", "node_modules", "__pycache__"}
+
+class GitCrawler(BaseModel):
+    repo_url : Optional[Any] = None
+    modules : Optional[List[str]] = None
+
+    def clone_repo(self, base_dir="repos"):
+        base_dir = Path(base_dir)
+        base_dir.mkdir(exist_ok=True)
+
+        name = self.repo_url.rstrip("/").split("/")[-1]
+        repo_path = base_dir / name
+
+        if repo_path.exists():
+            return repo_path
+
+        subprocess.run(
+            ["git", "clone", "--depth", "1", self.repo_url, str(repo_path)],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=False,
+        )
+        if not repo_path.exists():
+            return None
+
+        return repo_path
+
+    def find_main_modules(self):
+        repo_path = Path("repos") / self.repo_url.rstrip("/").split("/")[-1]
+        if not repo_path: return []
+        modules = []
+
+        for item in os.scandir(repo_path):
+            if item.is_dir() and item.name.lower() in TARGET_DIRS:
+                modules.append(Path(item.path))
+
+        self.modules = modules
+
+    def crawl_module(self,module_path):
+        collected = []
+
+        for root, dirs, files in os.walk(module_path):
+            dirs[:] = [d for d in dirs if d not in IGNORE_DIRS]
+
+            for f in files:
+                collected.append(Path(root) / f)
+
+        return collected
+
+    def crawl_repo_code(self):
+        if not self.modules:
+            self.find_main_modules()
+        all_files = []
+
+        for module in self.modules:
+            all_files.extend(self.crawl_module(module))
+
+        return all_files
